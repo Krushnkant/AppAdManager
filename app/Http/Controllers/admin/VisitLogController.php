78dc8595032d7c09a533f5225ad8040bc2b8b5bf
pankaj5604 <pankajahir631@gmail.com>
2023-02-27 12:09:18 +0530

    working 27-02-2023 16

--- app/Http/Controllers/admin/VisitLogController.php
+++ app/Http/Controllers/admin/VisitLogController.php
@@ -30,7 +30,8 @@ class VisitLogController extends Controller
             $app_id = $request->input('app_id');
 
             $totalData = users_apps_visit::select('user_id', \DB::raw('count(*) as total'))->join('users', 'users_apps_visits.user_id', '=', 'users.id')->where('users.app_id',$app_id);
-            $totalData = $totalData->groupBy('user_id')->get();
+            $totalData = $totalData->groupBy('user_id')->get()->toArray();
+            dd($totalData);
             $totalFiltered = count($totalData);
              //dd($totalFiltered);
             $limit = $request->input('length');
